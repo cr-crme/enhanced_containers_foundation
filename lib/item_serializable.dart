c93c344c8fb3e22f7d7ccb36b80caf1968cff0d0
Pariterre <pariterre@hotmail.com>
2025-04-09 16:49:55 -0400

    Using uuid instead of nanoid

--- lib/item_serializable.dart
+++ lib/item_serializable.dart
@@ -1,15 +1,17 @@
-import 'package:nanoid/nanoid.dart';
+import 'package:uuid/uuid.dart';
+
+final _uuid = const Uuid();
 
 /// The base class for the items contained in all the custom containers.
 ///
 /// Written by: @pariterre and @Guibi1
 abstract class ItemSerializable {
   /// Creates an [ItemSerializable] with the provided [id], or a randomly generated one.
-  ItemSerializable({String? id}) : id = id ?? nanoid();
+  ItemSerializable({String? id}) : id = id ?? _uuid.v4();
 
   /// Creates an [ItemSerializable] from a map of serialized items.
   ItemSerializable.fromSerialized(map)
-      : id = map != null && map['id'] != null ? map['id'] : nanoid();
+      : id = map != null && map['id'] != null ? map['id'] : _uuid.v4();
 
   /// Must be overriten to serialise additionnal information.
   Map<String, dynamic> serializedMap();
