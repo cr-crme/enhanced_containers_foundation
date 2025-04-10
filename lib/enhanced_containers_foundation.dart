5ff004163eb6b659ee3f3011cfa5f1cd1c240708
Pariterre <pariterre@hotmail.com>
2025-04-10 15:53:48 -0400

    Included more generic items

--- lib/enhanced_containers_foundation.dart
+++ lib/enhanced_containers_foundation.dart
@@ -1,1 +1,4 @@
 export 'package:enhanced_containers_foundation/item_serializable.dart';
+export 'package:enhanced_containers_foundation/item_serializable_with_creation_time.dart';
+export 'package:enhanced_containers_foundation/list_serializable.dart';
+export 'package:enhanced_containers_foundation/map_serializable.dart';
