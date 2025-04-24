e6b53889af13a50a434fa251aa57c5c511ee3363
Pariterre <pariterre@hotmail.com>
2025-04-24 17:17:34 -0400

    Added export

--- lib/enhanced_containers_foundation.dart
+++ lib/enhanced_containers_foundation.dart
@@ -1,3 +1,4 @@
+export 'package:enhanced_containers_foundation/exceptions.dart';
 export 'package:enhanced_containers_foundation/item_serializable.dart';
 export 'package:enhanced_containers_foundation/item_serializable_with_creation_time.dart';
 export 'package:enhanced_containers_foundation/list_serializable.dart';
